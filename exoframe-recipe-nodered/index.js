2b1acd373d1f9cb06cb9f9bd83cb2bfa906c26ea
Henrik Næss <henrik@heka.no>
2019-04-25 15:37:01 +0200

    Fixed bug where two questions had same name

--- exoframe-recipe-nodered/index.js
+++ exoframe-recipe-nodered/index.js
@@ -1,5 +1,6 @@
 const node_red_image = 'henrikcoll/nodered:latest';
 const bcrypt = require('bcrypt-nodejs')
+const generateName = require('./generateName')
 
 exports.getQuestions = () => [
   {
@@ -10,7 +11,7 @@ exports.getQuestions = () => [
   {
     type: 'input',
     name: 'url',
-    message: 'Instance url:',
+    message: 'Instance url :',
   },
   {
     type: 'input',
@@ -32,36 +33,72 @@ exports.getQuestions = () => [
   },
   {
     type: 'confirm',
-    name: 'enable_projects',
+    name: 'enable_context_file_storage',
     message: 'Do you want to enable context file storage?',
     default: false
   },
+  {
+    type: 'input',
+    name: 'deployments',
+    message: 'How many deployments do you want?',
+    default: 1
+  },
 ];
 
-const start_node_red = async ({util, answers, username, docker}) => {
-  const deploymentName = util.nameFromImage(node_red_image);
-
+const start_node_red = async ({username, docker, conf}) => {
   return await docker.startFromParams({
     image: node_red_image,
-    projectName: answers.name,
+    projectName: conf.name,
     username,
-    deploymentName,
-    frontend: `Host:${answers.url}`,
+    deploymentName: conf.name,
+    frontend: `Host:${conf.url}`,
     restartPolicy: 'always',
     Mounts: [
       {
         Type: 'volume',
-        Source: `${answers.name}-data`,
+        Source: `${conf.name}-data`,
         Target: '/data',
       },
     ],
-    Env: [`ADMIN_USERNAME=${answers.admin_username}`, `ADMIN_PASSWORD=${bcrypt.hashSync(answers.admin_password)}`, `PORT=80`, `ENABLE_PROJECTS=${answers.enable_projects}`, `ENABLE_FILE_CONTEXT_STORAGE=${answers.enable_context_file_storage}`],
+    Env: [`ADMIN_USERNAME=${conf.admin_username}`, `ADMIN_PASSWORD=${bcrypt.hashSync(conf.admin_password)}`, `PORT=80`, `ENABLE_PROJECTS=${conf.enable_projects}`, `ENABLE_FILE_CONTEXT_STORAGE=${conf.enable_context_file_storage}`],
   });
 };
+
+function replaceStrings(str, index) {
+  return str.replace(/\{([^\}]+)\}/g, (match, p1, offset, string) => {
+    if (p1.match(/random\.number/)) {
+      let m = p1.match(/:(\d)+/)
+      let count = m?parseInt(m[1]):5
+
+      let random = Math.floor(Math.random() * (parseInt("9".repeat(count)) - 0) + 0)
+      random = "0".repeat(count - ("" + random).length) + random
+      return random
+
+    } else if (p1.match(/random\.string/)) {
+      let m = p1.match(/:(\d)+/)
+      return Math.random().toString(36).substring(m?parseInt(m[1]):5)
+
+    } else if (p1 === "index") {
+      return index
+
+    } else if (p1.match(/random\.name/)) {
+      let m = p1.match(/:(\d)+/)
+      return generateName({
+        wordCount: m?parseInt(m[1]):2,
+      })
+
+    }
+    return ""
+  })
+}
 
 exports.runSetup = async ({answers, serverConfig, username, docker, util}) => {
   // init log
   const log = [];
+
+  let numDeployments = parseInt(answers.deployments)
+  if (!(numDeployments && numDeployments > 0))
+    throw new Error('Deployments must be a number greater than 0')
 
   try {
     util.logger.debug('starting work..');
@@ -69,12 +106,24 @@ exports.runSetup = async ({answers, serverConfig, username, docker, util}) => {
       util.logger.debug('pulling node-red..');
       await docker.pullImage(node_red_image);
     }
+    
+    let confs = [];
 
-    util.logger.debug('starting node-red..');
-    const node_red = await start_node_red({util, answers, username, docker});
-    log.push({message: 'node-red container started', data: node_red, level: 'info'});
-    util.logger.debug('created node-red container..');
-
+      for (var i = 0; i < numDeployments; i++) {
+        let conf = {
+          name: replaceStrings(answers.name, i),
+          url: replaceStrings(answers.url, i),
+          admin_username: answers.admin_username,
+          admin_password: answers.admin_password,
+          enable_projects: answers.enable_projects,
+          enable_context_file_storage: answers.enable_context_file_storage
+        }
+        util.logger.debug(`starting node-red (${conf.name})..`);
+        const node_red = await start_node_red({username, docker, conf});
+        log.push({message: `node-red container started (${conf.name})`, data: node_red, level: 'info'});
+        util.logger.debug(`created node-red container (${conf.name})..`);
+        confs.push(conf)
+      }
   } catch (e) {
     util.logger.error('error:', e);
     log.push({message: e.toString(), data: e, level: 'error'});
